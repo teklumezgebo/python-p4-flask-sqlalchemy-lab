efabe54ad899735010a9dbbc7573078d9f7fc5b1
Teklu Mezgebo <mezge002@umn.edu>
2024-01-26 21:59:39 -0600

    lab completed. 3 models created. all models migrated along with seed data being seeded. Created 3 routes created

--- server/app.py
+++ server/app.py
@@ -19,16 +19,44 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    return f'''
+            <ul>ID: {animal.id}</ul>
+            <ul>Name: {animal.name}</ul>
+            <ul>Species: {animal.species}</ul>
+            <ul>Zookeeper: {animal.zookeeper}</ul>
+            <ul>Enclosure: {animal.enclosure}</ul>
+        '''
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    zookeeper_animals = ''
+
+    for animal in zookeeper.animals:
+        zookeeper_animals += f'<ul>Animal: {animal.name}</ul>'
+    
+    return f'''
+            <ul>ID: {zookeeper.id}</ul>
+            <ul>Name: {zookeeper.name}</ul>
+            <ul>Birthday: {zookeeper.birthday}</ul>
+            {zookeeper_animals}
+        '''
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    enclosure_animals = ''
 
+    for animal in enclosure.animals:
+        enclosure_animals += f'<ul>Animal: {animal.name}</ul>'
+
+    return f'''
+            <ul>ID: {enclosure.id}</ul>
+            <ul>Environment: {enclosure.environment}</ul>
+            <ul>Open to visitors {enclosure.open_to_visitors}</ul>
+            {enclosure_animals}
+        '''
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
